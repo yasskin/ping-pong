61ee89e91a4ffd24f5f278bd8c458da1327b89b3
Noah Yasskin <yasskin@icloud.com>
2016-06-10 10:55:04 -0700

    add some css styling and clean up the code after

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,9 @@
 // Ping-Pong Back End (Business) Logic
-
 var arrayGenerator = function(pongInput) {
-
-  var arrayNumbers = [ ];
-
-  for (var i = 1; i <= pongInput; i +=1) {
-    arrayNumbers = [ ];
-    };
 
   var arrayPingPong = [ ];
 
-// creates the array with every number from 1 to the User Input
   for (var index = 1; index <= pongInput; index += 1) {
-    // function pingModulo(index) {
-    //   return (index % 3);
-    // }
-    // var pingInsert = pingModulo(index);
-    // console.log(pingInsert);
 
     if (index % 15 === 0) {
       arrayPingPong.push("ping-pong");
@@ -30,32 +17,15 @@ var arrayGenerator = function(pongInput) {
     else {
       arrayPingPong.push(index);
     }
-
   };
-
-  // var modulo = index % 3;
-  //    if (modulo = 0) { };
-
-// Rule: A number is divisible by 3 if the sum of its digits is divisible by 3.
-
-//  for (var index = 1; index <= pongInput; index += 1) {
-//    if (index % 3 = 0) {
-//      arrayNumbers.push[index] = "ping";
-//    };
-//  };
-
-//  Rule: A number is divisible by 5 if the its last digit is a 0 or 5.
-
   return arrayPingPong;
-
 };
 
 // Ping-Pong Front End (User Interface) Logic
-
 $(document).ready(function() {
   $("form#pong-form").submit(function(event) {
     event.preventDefault();
-//    $("ul#ping-pong-results li").remove();
+    $("ul#pong-results li").remove();
     var pongInput = parseInt($("input#pong-input").val());
 
     var pongResults = arrayGenerator(pongInput);
@@ -66,6 +36,5 @@ $(document).ready(function() {
     pongResults.forEach(function(result) {
       $("#pong-results").append("<li>" + result + "</li>");
     });
-
   });
 });
